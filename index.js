88ec7e6cca9f8a3e181043e510ae7b51685c9e23
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2015-07-25 15:50:38 +0200

    feat: add bluebrd, lodash, monet and shelljs

--- index.js
+++ index.js
@@ -1,16 +1,56 @@
+/* eslint quotes: [0, "double"], strict: [0] */
+
 "use strict";
 
 var shelljs = require("shelljs");
 var promise = require("bluebird");
 var _ = require("lodash");
+var fs = promise.promisifyAll(require("fs"));
+var monet = require("monet");
+var path = require("path");
+var $m = monet.Maybe.fromNull;
 
-var _module = function () {
+var _require = require("docopt");
 
-  var foo = function () {};
+var docopt = _require.docopt;
 
-  return {
-    foo: foo
-  };
+function doMaybe(gen) {
+    "use strict";
+
+    function step(value) {
+        var result = gen.next(value);
+        if (result.done) {
+            return result.value;
+        }
+        return result.value.bind(step);
+    }
+    return step();
+}
+
+var getOption = function (a, b, def, o) {
+    "use strict";
+    return $m(o[a]).orElse($m(o[b])).orSome(def);
 };
 
-module.exports = _module();
+var mod = function () {
+
+    return {
+        $s: promise.promisifyAll(shelljs),
+        $b: promise,
+        Promise: promise,
+        _: _,
+        $d: docopt,
+        $o: getOption,
+
+        $mMaybe: monet.Maybe,
+        $mDoMaybe: doMaybe,
+        $m: monet.Maybe.fromNull,
+        $f: {
+            readLocal: function (f) {
+                return fs.readFileAsync(path.join(__dirname, "/" + f), "utf8");
+            }
+        }
+    };
+};
+
+module.exports = mod();
--- src/index.js
+++ src/index.js
@@ -1,16 +1,54 @@
-var shelljs = require( 'shelljs' )
-var promise = require( 'bluebird' )
-var _ = require( 'lodash' )
+/* eslint quotes: [0, "double"], strict: [0] */
 
-var _module = () => {
+var shelljs = require('shelljs')
+var promise = require('bluebird')
+var _		= require('lodash')
+var fs		= promise.promisifyAll(require('fs'))
+var monet	= require('monet')
+var path	= require('path')
+var $m		= monet.Maybe.fromNull
 
-  var foo = () => {
+var {
+    docopt
+}			= require('docopt')
 
-  }
+function doMaybe(gen) {
+    "use strict"
 
-  return {
-    foo
-  }
+    function step(value) {
+        var result = gen.next(value);
+        if (result.done) {
+            return result.value;
+        }
+        return result.value.bind(step);
+    }
+    return step();
 }
 
-module.exports = _module()
+var getOption = (a, b, def, o) => {
+    "use strict"
+    return $m(o[a])
+        .orElse($m(o[b]))
+        .orSome(def)
+}
+
+var mod = () => {
+
+    return {
+        $s: promise.promisifyAll(shelljs),
+        $b: promise,
+        Promise: promise,
+        _: _,
+        $d: docopt,
+        $o: getOption,
+
+        $mMaybe: monet.Maybe,
+        $mDoMaybe: doMaybe,
+        $m: monet.Maybe.fromNull,
+        $f: {
+            readLocal: (f) => fs.readFileAsync(path.join(__dirname, `/${f}`), 'utf8')
+        }
+    }
+}
+
+module.exports = mod()
